f1ea44f5ef6cf3c5d8becc5b6cb92ad889d0c1b8
Shridhar <shridharsapkal1996@gmail.com>
2026-06-12 23:45:42 +0530

    Refractor the project structure. CSVReaderUtil:load the CSV file and map to UserBean
    Userbean: Bean class for CSV mapping and will be used by DataProviders
    
    Moved the testData from src/main/resources to src/test/resources

--- src/test/java/com/api/utils/CSVReaderUtil.java
+++ src/test/java/com/api/utils/CSVReaderUtil.java
@@ -1,37 +1,50 @@
-package com.demo.csv;
+package com.api.utils;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.List;
 
+import com.dataprovider.api.bean.UserBean;
 import com.opencsv.CSVReader;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
 import com.opencsv.exceptions.CsvException;
 
-public class ReadCSVFile_MapToPOJO {
+public class CSVReaderUtil {
 
-	public static void main(String[] args) throws IOException, CsvException {
+	/*
+	 * 
+	 * Constructor is private
+	 *
+	 * static static methods job Help me read the CSV file and Map it a Bean
+	 *
+	 *
+	 */
+
+	private CSVReaderUtil() {
+		// No one can create Object of CSVReaderUtil OutSide the class
+		// Singlton classs constructor are private
+
+	}
+
+	public static void LocalCSV(String pathOfCSVFile) throws IOException, CsvException {
 		// Code to read the CSV file in java !!! [Important interview questions]
 
 		InputStream is = Thread.currentThread().getContextClassLoader().getResourceAsStream("testData/LoginCreds.csv");
 		InputStreamReader isr = new InputStreamReader(is);
 
-
 		CSVReader csvReader = new CSVReader(isr); // CSVReader Constructor //Require a reader !!
-		
-		//Write thge code to map CSV to POJO
-		//Its convered csv to object 
-		CsvToBean<UserBean> csvToBean=new CsvToBeanBuilder(csvReader)
+
+	
+		CsvToBean<UserBean> csvToBean = new CsvToBeanBuilder(csvReader)
 				.withType(UserBean.class)
-				.withIgnoreEmptyLine(true) 
-				.build();  
-		
-		
-		List<UserBean> userList=csvToBean.parse();
+				.withIgnoreEmptyLine(true)
+				.build();
+
+		List<UserBean> userList = csvToBean.parse();
 		System.out.println(userList);
 
-			}
+	}
 
 }
--- src/test/java/com/dataprovider/api/bean/UserBean.java
+++ src/test/java/com/dataprovider/api/bean/UserBean.java
@@ -1,4 +1,4 @@
-package com.demo.csv;
+package com.dataprovider.api.bean;
 
 
 import com.opencsv.bean.CsvBindByName;
